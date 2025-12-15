2a751ee6a19298f9bc17e2f22574027d62104c60
Ray0831032 <rg1107369@gmail.com>
2025-12-15 17:33:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_member_join(member):
-    await asyncio.sleep(3)
+    await asyncio.sleep(0.1)
     role = discord.utils.get(member.guild.roles, name="未驗證")
     if role:
         await member.add_roles(role)
